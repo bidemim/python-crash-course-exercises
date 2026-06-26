ea497e977efafd4bfbbe72462549f2a0d7540810
Bidemi Muibudeen <251024993+bidemim@users.noreply.github.com>
2026-06-26 14:37:54 +0200

    update city function with an optional argument

--- Chapter_11/city_function.py
+++ Chapter_11/city_function.py
@@ -1,4 +1,7 @@
-def get_city_and_country(city, country, population):
+def get_city_and_country(city, country, population=""):
     """Get city, country and population from user."""
-    city_details = f"{city}.title(), {country}.title() - population {population}"
-    return city_details
+    if population:
+        city_details = f"{city}.title(), {country}.title() - population {population}"
+    else:
+        city_details = f"{city}.title(), {country}.title()"
+        return city_details
