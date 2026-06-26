aa86be3f061e7e8c15ced5603f5263954b6e814a
Bidemi Muibudeen <251024993+bidemim@users.noreply.github.com>
2026-06-26 14:29:06 +0200

    add new population parameter

--- Chapter_11/city_function.py
+++ Chapter_11/city_function.py
@@ -1,4 +1,4 @@
-def get_city_and_country(city, country):
+def get_city_and_country(city, country, population):
     """Get city and country names from user."""
     city_name = f"{city}, {country}"
     return city_name.title()
