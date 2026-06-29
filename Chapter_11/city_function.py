9636fbc24f77705d1b1e7750ea1785287c4ffd16
Bidemi Muibudeen <251024993+bidemim@users.noreply.github.com>
2026-06-29 16:49:42 +0200

    capitalize Population

--- Chapter_11/city_function.py
+++ Chapter_11/city_function.py
@@ -1,7 +1,7 @@
 def get_city_and_country(city, country, population=""):
     """Get city, country and population from user."""
     if population:
-        city_details = f"{city}.title(), {country}.title() - population {population}"
+        city_details = f"{city}.title(), {country}.title() - Population {population}"
     else:
         city_details = f"{city}.title(), {country}.title()"
         return city_details
