07b5f5e438c926988eb16ad764abddcb3f731af9
Bidemi Muibudeen <251024993+bidemim@users.noreply.github.com>
2026-06-29 23:02:44 +0200

    add new test for population

--- Chapter_11/test_cities.py
+++ Chapter_11/test_cities.py
@@ -4,3 +4,8 @@ def test_city_country_name():
     """Would a city like 'Santiago, Chile' work?'"""
     city_country = get_city_and_country("santiago", "chile")
     assert city_country == "Santiago, Chile"
+
+def test_city_country_population_name():
+    """Would details like 'New York, USA, 7 million work?'"""
+    city_country = get_city_and_country("new york", 'usa', '7 million')
+    assert city_country == "New York, USA, 7 million"
