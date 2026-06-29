27afcfc9ce91018e03163f9386d6daf854de09aa
Bidemi Muibudeen <251024993+bidemim@users.noreply.github.com>
2026-06-29 23:46:53 +0200

    move abbreviation test case

--- Chapter_11/city_function.py
+++ Chapter_11/city_function.py
@@ -2,6 +2,9 @@ def get_city_and_country(city, country, population=""):
     """Get city, country and population from user."""
     if population:
         city_details = f"{city.title()}, {country.title()} - Population {population}"
+    elif population == "usa" or "uk" or "uae":
+        population.capitalize()
+
     else:
         city_details = f"{city.title()}, {country.title()}"
     return city_details
