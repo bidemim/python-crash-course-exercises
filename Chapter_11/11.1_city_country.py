b2704fc0d77fff30a212011801454378353b99e0
Bidemi Muibudeen <251024993+bidemim@users.noreply.github.com>
2026-07-17 07:39:58 +0200

    rewrote city functions

--- Chapter_11/11.1_city_country.py
+++ Chapter_11/11.1_city_country.py
@@ -1,21 +1,4 @@
-from city_function import get_city_and_country
-
-print("Enter 'e' anytime to exit.")
-
-while True:
-    city = input("\nPlease enter city name: ")
-    if city == 'e':
-        break
-
-    country = input("Please enter the country: ")
-    if country == 'e':
-        break
-
-    population = input("Please enter the city's population: ")
-    if population == 'e':
-        break
-    elif population == "usa" or "uk" or "uae":
-        population.capitalize()
-
-    city_country = get_city_and_country(city, country, population)
-    print(f"\tCity and Country name: {city_country}")
+def city_functions(city, country):
+    """Return the details about a city."""
+    city_details = f"{city} {country}"
+    return city_details.title()
